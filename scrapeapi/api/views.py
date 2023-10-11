7021cae3edabd5144d25a52a548811b26c5bbb13
andviktor <and.viktor@gmail.com>
2023-10-11 13:55:30 +0700

    Token permissions added to api

--- scrapeapi/api/views.py
+++ scrapeapi/api/views.py
@@ -1,15 +1,19 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 from .models import Project, Scraper, Element
 from .serializers import ProjectSerializer, ScraperSerializer, ElementSerializer
 
 class ProjectViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication, )
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
 class ScraperViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication, )
     queryset = Scraper.objects.all()
     serializer_class = ScraperSerializer
 
 class ElementViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication, )
     queryset = Element.objects.all()
     serializer_class = ElementSerializer
