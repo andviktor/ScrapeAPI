b74dda944c9ffdfe463a3babb714b4a46e3ad139
andviktor <and.viktor@gmail.com>
2023-10-12 16:10:31 +0700

    Models updated.

--- scrapeapi/api/models.py
+++ scrapeapi/api/models.py
@@ -4,15 +4,21 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=300, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, default=None)
+
+    def __str__(self) -> str:
+        return self.title
 
 class Scraper(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=300, blank=True)
     output_json = models.TextField(blank=True)
-    exec_datetime = models.DateTimeField()
+    exec_datetime = models.DateTimeField(null=True, blank=True)
     headers = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.title
 
 class Element(models.Model):
     scraper = models.ForeignKey(Scraper, on_delete=models.CASCADE)
@@ -21,3 +27,6 @@ class Element(models.Model):
     regex_sub = models.TextField(blank=True)
     regex_search = models.TextField(blank=True)
     concat_result = models.CharField(max_length=10, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
